3e1e8d264bfb8c4a82b97328d99f3ffcb8172a82
Christian Llontop <me@chrisllontop.com>
2025-05-28 17:27:47 -0500

    Refactor exports in index.ts for improved module organization (#12)

--- packages/fastify-file-routes/src/index.ts
+++ packages/fastify-file-routes/src/index.ts
@@ -1,7 +1,14 @@
+// Export the main plugin
+export { fastifyFileRoutes } from "./plugin.js";
+
+// Export types
+export type {
+  FileRoutesOptions,
+  RouteHandler,
+  HttpMethod,
+  RouteSchema,
+} from "./types.js";
+
+// Export default for convenience
 import { fastifyFileRoutes } from "./plugin.js";
-import type { FileRoutesOptions, HttpMethod, RouteHandler, RouteSchema } from "./types.js";
-
-export { fastifyFileRoutes };
-export type { FileRoutesOptions, RouteHandler, HttpMethod, RouteSchema };
-
 export default fastifyFileRoutes;
