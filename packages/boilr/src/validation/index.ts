5b83a040b6c4d9d04558d1ae037850d573e56024
Eduardo Valderrama <edu.valderrama.suarez@gmail.com>
2025-08-15 15:15:53 -0500

    update version of zod

--- packages/boilr/src/validation/index.ts
+++ packages/boilr/src/validation/index.ts
@@ -1,4 +1,4 @@
-import type { FastifySchema } from "fastify";
+import type { FastifySchema, RouteOptions } from "fastify";
 import {
   ZodTypeProvider,
   jsonSchemaTransform as baseJsonSchemaTransform,
@@ -6,16 +6,26 @@ import {
   serializerCompiler,
   validatorCompiler,
 } from "fastify-type-provider-zod";
+import type { OpenAPIV3, OpenAPIV3_1 } from "openapi-types";
 
 type TransformResult = ReturnType<typeof baseJsonSchemaTransform> & {
   tags?: string[];
 };
 
-// Extended transform function that handles Swagger tags
-function jsonSchemaTransform({ schema, url }: { schema: FastifySchema; url: string }): TransformResult {
-  // First, apply the base transformation
-  const transformed = baseJsonSchemaTransform({ schema, url });
+type RouteContext = {
+    schema: FastifySchema;
+    url: string;
+    route: RouteOptions;
+    openapiObject?: Partial<OpenAPIV3.Document | OpenAPIV3_1.Document>;
+}
 
+// Extended transform function that handles Swagger tags
+function jsonSchemaTransform({ schema, url, route, openapiObject }: RouteContext): TransformResult {
+  if (!openapiObject) {
+    throw new Error("openapiObject is missing in the transform context. This should not happen.");
+  }
+  // First, apply the base transformation
+  const transformed = baseJsonSchemaTransform({ schema, url, route, openapiObject });
   // Check if the schema has tags and include them in the transformation
   if (schema && typeof schema === "object" && "tags" in schema) {
     const tags = schema.tags;
